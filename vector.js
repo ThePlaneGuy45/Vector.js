da3b72c099d4167a65f063a98d1fca131b3b3046
ThePlaneGuy45 <69761838+ThePlaneGuy45@users.noreply.github.com>
2022-12-01 10:52:09 -0500

    Version 2.0.0!
    - Removed the need for () after swizzling
    - Remade into class, not object
    - Added vector exponentiation

--- vector.js
+++ vector.js
@@ -1,244 +1,133 @@
-/* Vector Defining *****************************************************/
+/* Vector2 Class ***********************/
 
-const Vector2 = function(x, y){
-    this.x = x;
-    this.y = y;
-}
-const vector2 = Vector2;
-const Vec2 = Vector2;
-const vec2 = Vector2;
-
-const Vector3 = function(x, y, z){
-    this.x = x;
-    this.y = y;
-    this.z = z;
-};
-const vector3 = Vector3;
-const Vec3 = Vector3;
-const vec3 = Vector3;
-
-/* Vec2 Swizzling *****************************************************/
-
-Vector2.prototype.xx = function() { new Vector2(this.x, this.x); }
-Vector2.prototype.xy = function() { new Vector2(this.x, this.y); }
-Vector2.prototype.yx = function() { new Vector2(this.y, this.x); }
-Vector2.prototype.yy = function() { new Vector2(this.y, this.y); }
-Vector2.prototype.xxx = function() { return new Vector3(this.x, this.x, this.x); }
-Vector2.prototype.xxy = function() { return new Vector3(this.x, this.x, this.y); }
-Vector2.prototype.xyx = function() { return new Vector3(this.x, this.y, this.x); }
-Vector2.prototype.xyy = function() { return new Vector3(this.x, this.y, this.y); }
-Vector2.prototype.yxx = function() { return new Vector3(this.y, this.x, this.x); }
-Vector2.prototype.yxy = function() { return new Vector3(this.y, this.x, this.y); }
-Vector2.prototype.yyx = function() { return new Vector3(this.y, this.y, this.x); }
-Vector2.prototype.yyy = function() { return new Vector3(this.y, this.y, this.y); }
-
-/* Vector2 Math *****************************************************/
-
-Vector2.prototype.add = function(b) {
-    return new Vector2(x + b.x, y + b.y);
-}
-
-Vector2.prototype.sub = function(b) {
-    return new Vector2(x - b.x, y - b.y);
-}
-Vector2.prototype.subtract = Vector2.prototype.sub;
-
-Vector2.prototype.scale = function(b) {
-    return new Vector2(x * b, y * b);
-}
-
-Vector2.prototype.mul = function(b) {
-    return new Vector2(x * b.x, y * b.y);
-}
-Vector2.prototype.multiply = Vector2.prototype.mul;
-
-Vector2.prototype.div = function(b) {
-    return new Vector2(x / b.x, y / b.y);
-}
-Vector2.prototype.divide = Vector2.prototype.div;
-
-Vector2.prototype.len = function() {
-    return Math.sqrt(x * x + y * y);
-}
-Vector2.prototype.length = Vector2.prototype.len;
-
-Vector2.prototype.dist = function(b) {
-    return this.sub(b).len;
-}
-Vector2.prototype.distance = Vector2.prototype.dist;
-
-Vector2.prototype.norm = function() {
-    let l = new Vector2(x, y).len();
-    return new Vector2(x / l, y / l);
-}
-Vector2.prototype.normalize = Vector2.prototype.norm;
-
-Vector2.prototype.dot = function(b) {
-    return x * b.x + y * b.y;
-}
-
-Vector2.prototype.lerp = function(b, t) {
-    return new Vector2(
-        x + (b.x - x) * t,
-        y + (b.y - y) * t
-    )
-}
-Vector2.prototype.interpolate = Vector2.prototype.lerp;
-
-/* Vec2 Non-math Functions *****************************************************/
-
-Vector2.prototype.toArray = function() {
-    return [this.x, this.y];
-}
-
-Vector2.prototype.toString = function() {
-    return ("(" + this.x + ", " + this.y + ")");
-}
-
-Vector2.prototype.set = function(x, y) {
-    return new Vector2(x, y);
-}
-
-/* Vec3 Swizzling *****************************************************/
-
-Vector3.prototype.xx = function() { return new Vector2(this.x, this.x); }
-Vector3.prototype.xy = function() { return new Vector2(this.x, this.y); }
-Vector3.prototype.xz = function() { return new Vector2(this.x, this.z); }
-Vector3.prototype.yx = function() { return new Vector2(this.y, this.x); }
-Vector3.prototype.yy = function() { return new Vector2(this.y, this.y); }
-Vector3.prototype.yz = function() { return new Vector2(this.y, this.z); }
-Vector3.prototype.zx = function() { return new Vector2(this.z, this.x); }
-Vector3.prototype.zy = function() { return new Vector2(this.z, this.y); }
-Vector3.prototype.zz = function() { return new Vector2(this.z, this.z); }
-Vector3.prototype.xxx = function() { return new Vector3(this.x, this.x, this.x); }
-Vector3.prototype.xxy = function() { return new Vector3(this.x, this.x, this.y); }
-Vector3.prototype.xxz = function() { return new Vector3(this.x, this.x, this.z); }
-Vector3.prototype.xyx = function() { return new Vector3(this.x, this.y, this.x); }
-Vector3.prototype.xyy = function() { return new Vector3(this.x, this.y, this.y); }
-Vector3.prototype.xyz = function() { return new Vector3(this.x, this.y, this.z); }
-Vector3.prototype.xzx = function() { return new Vector3(this.x, this.z, this.x); }
-Vector3.prototype.xzy = function() { return new Vector3(this.x, this.z, this.y); }
-Vector3.prototype.xzz = function() { return new Vector3(this.x, this.z, this.z); }
-Vector3.prototype.yyx = function() { return new Vector3(this.y, this.y, this.x); }
-Vector3.prototype.yyy = function() { return new Vector3(this.y, this.y, this.y); }
-Vector3.prototype.yyz = function() { return new Vector3(this.y, this.y, this.z); }
-Vector3.prototype.yxx = function() { return new Vector3(this.y, this.x, this.x); }
-Vector3.prototype.yxy = function() { return new Vector3(this.y, this.x, this.y); }
-Vector3.prototype.yxz = function() { return new Vector3(this.y, this.x, this.z); }
-Vector3.prototype.yzx = function() { return new Vector3(this.y, this.z, this.x); }
-Vector3.prototype.yzy = function() { return new Vector3(this.y, this.z, this.y); }
-Vector3.prototype.yzz = function() { return new Vector3(this.y, this.z, this.z); }
-Vector3.prototype.zxx = function() { return new Vector3(this.z, this.x, this.x); }
-Vector3.prototype.zxy = function() { return new Vector3(this.z, this.x, this.y); }
-Vector3.prototype.zxz = function() { return new Vector3(this.z, this.x, this.z); }
-Vector3.prototype.zyx = function() { return new Vector3(this.z, this.y, this.x); }
-Vector3.prototype.zyy = function() { return new Vector3(this.z, this.y, this.y); }
-Vector3.prototype.zyz = function() { return new Vector3(this.z, this.y, this.z); }
-Vector3.prototype.zzx = function() { return new Vector3(this.z, this.z, this.x); }
-Vector3.prototype.zzy = function() { return new Vector3(this.z, this.z, this.y); }
-Vector3.prototype.zzz = function() { return new Vector3(this.z, this.z, this.z); }
-
-/* Vector3 Math *****************************************************/
-
-Vector3.prototype.add = function(b) {
-    return new Vector3(x + b.x, y + b.y, z + b.z);
-}
-
-Vector3.prototype.sub = function(b) {
-    return new Vector3(x - b.x, y - b.y, z - b.z);
-}
-Vector3.prototype.subtract = Vector3.prototype.sub;
-
-Vector3.prototype.scale = function(b) {
-    return new Vector3(x * b, y * b, z * b);
-}
-
-Vector3.prototype.mul = function(b) {
-    return new Vector3(x * b.x, y * b.y, z * b.z);
-}
-Vector3.prototype.multiply = Vector3.prototype.mul;
-
-Vector3.prototype.div = function(b) {
-    return new Vector3(x / b.x, y / b.y, z / b.z);
-}
-Vector3.prototype.divide = Vector3.prototype.div;
-
-Vector3.prototype.len = function() {
-    return Math.sqrt(x * x + y * y + z * z);
-}
-Vector3.prototype.length = Vector3.prototype.len;
-
-Vector3.prototype.dist = function(b) {
-    return Vector3.prototype.sub(b).len;
-}
-Vector3.prototype.distance = Vector3.prototype.dist;
-
-Vector3.prototype.norm = function() {
-    let l = new Vector3(x, y, z).len();
-    return new Vector3(x / l, y / l, z / l);
-}
-Vector3.prototype.normalize = Vector3.prototype.norm;
-
-Vector3.prototype.dot = function(b) {
-    return x * b.x + y * b.y + z * b.z;
-}
-
-Vector3.prototype.lerp = function(b, t) {
-    return new Vector3(
-        x + (b.x - x) * t,
-        y + (b.y - y) * t,
-        z + (b.z - z) * t
-    )
-}
-Vector3.prototype.interpolate = Vector3.prototype.lerp;
-
-Vector3.prototype.cross = function(b) {
-    return new Vector3(
-        y * z - z * y,
-        z * x - x * z,
-        x * y - y * x
-    );
-}
-
-Vector3.prototype.reflect = function(n) {
-    let p = 2 * Vector3.prototype.dot(n);
-    return new Vector3 (
-        x - p * n.x,
-        y - p * n.y,
-        z - p * n.z
-    );
-}
-
-/* Non-math Vec3 Functions *****************************************************/
-
-Vector3.prototype.toArray = function() {
-    return [this.x, this.y, this.z];
-}
-
-Vector3.prototype.toString = function() {
-    return ("(" + this.x + ", " + this.y + ", " + this.z + ")");
-}
-
-Vector3.prototype.set = function(x, y, z) {
-    return new Vector3(x, y, z)
-}
-
-/* Array Functions *****************************************************/
-
-Array.prototype.toVector = function() {
-    if (this.length == 2) {
-        return new Vector2(
-            this[0],
-            this[1]
-        );
-    } else if (this.length == 3) {
-        return new Vector3(
-            this[0],
-            this[1],
-            this[2]
-        );
-    } else {
-        console.error("Invalid Array Length");
-        return(0);
+class Vector2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
+    
+    /* Swizzeling *******/
+    get xx() { return new Vector2(this.x, this.x); }
+    get xy() { return new Vector2(this.x, this.y); }
+    get yx() { return new Vector2(this.y, this.x); }
+    get yy() { return new Vector2(this.y, this.y); }
+    get xxx() { return new Vector3(this.x, this.x, this.x); }
+    get xxy() { return new Vector3(this.x, this.x, this.y); }
+    get xyx() { return new Vector3(this.x, this.y, this.x); }
+    get xyy() { return new Vector3(this.x, this.y, this.y); }
+    get yxx() { return new Vector3(this.y, this.x, this.x); }
+    get yxy() { return new Vector3(this.y, this.x, this.y); }
+    get yyx() { return new Vector3(this.y, this.y, this.x); }
+    get yyy() { return new Vector3(this.y, this.y, this.y); }
+
+    /* Math Functions *******/
+    add(b) { this.x += b.x; this.y += b.y; return this; }
+    sub(b) { this.x -= b.x; this.y -=b.y; return this; }
+    subtract(b) { return this.sub(b); }
+    scale(b) { this.x *= b; this.y *= b; return this; }
+    mul(b) { this.x *= b.x; this.y *= b.y; return this; }
+    multiply(b) { return this.mul(b); }
+    div(b) { this.x /= b.x; this.y /= b.y; return this; }
+    divide(b) { return this.div(b); }
+    exp(b) { this.x **= b.x; this.y **= b.y; return this; }
+    pow(b) { return this.exp(b); }
+    len() { return Math.sqrt( this.x * this.x + this.y * this.y ); }
+    length() { return this.len(); }
+    dist(b) { return this.sub(b).len(); }
+    distance(b) { return this.distance(b); }
+    norm() { let l = this.len(); return this.div(new Vector2(l, l)); }
+    normalize() { return this.norm(); }
+    dot(b) { return this.x * b.x + this.y * b.y; }
+    lerp(b, t) { return this.add(b.sub(this).scale(t)); }
+    interpolate(b, t) { return this.lerp(b, t); }
+
+    /* Non-Math Functions *******/
+    toArray() { return [this.x, this.y]; }
+    toString() { return ("(" + this.x + ", " + this.y + ")"); }
+    set(x, y) { this.x = x; this.y = y; }
 }
+vector2 = Vector2; // Aliases
+Vec2 = Vector2;
+vec2 = Vector2;
+
+/* Vector3 Class ***********************/
+
+class Vector3 {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /* Swizzeling *******/
+    get xx() { return new Vector2(this.x, this.x); }
+    get xy() { return new Vector2(this.x, this.y); }
+    get xz() { return new Vector2(this.x, this.z); }
+    get yx() { return new Vector2(this.y, this.x); }
+    get yy() { return new Vector2(this.y, this.y); }
+    get yz() { return new Vector2(this.y, this.z); }
+    get zx() { return new Vector2(this.z, this.x); }
+    get zy() { return new Vector2(this.z, this.y); }
+    get zz() { return new Vector2(this.z, this.z); }
+    get xxx() { return new Vector3(this.x, this.x, this.x); }
+    get xxy() { return new Vector3(this.x, this.x, this.y); }
+    get xxz() { return new Vector3(this.x, this.x, this.z); }
+    get xyx() { return new Vector3(this.x, this.y, this.x); }
+    get xyy() { return new Vector3(this.x, this.y, this.y); }
+    get xyz() { return new Vector3(this.x, this.y, this.z); }
+    get xzx() { return new Vector3(this.x, this.z, this.x); }
+    get xzy() { return new Vector3(this.x, this.z, this.y); }
+    get xzz() { return new Vector3(this.x, this.z, this.z); }
+    get yyx() { return new Vector3(this.y, this.y, this.x); }
+    get yyy() { return new Vector3(this.y, this.y, this.y); }
+    get yyz() { return new Vector3(this.y, this.y, this.z); }
+    get yxx() { return new Vector3(this.y, this.x, this.x); }
+    get yxy() { return new Vector3(this.y, this.x, this.y); }
+    get yxz() { return new Vector3(this.y, this.x, this.z); }
+    get yzx() { return new Vector3(this.y, this.z, this.x); }
+    get yzy() { return new Vector3(this.y, this.z, this.y); }
+    get yzz() { return new Vector3(this.y, this.z, this.z); }
+    get zxx() { return new Vector3(this.z, this.x, this.x); }
+    get zxy() { return new Vector3(this.z, this.x, this.y); }
+    get zxz() { return new Vector3(this.z, this.x, this.z); }
+    get zyx() { return new Vector3(this.z, this.y, this.x); }
+    get zyy() { return new Vector3(this.z, this.y, this.y); }
+    get zyz() { return new Vector3(this.z, this.y, this.z); }
+    get zzx() { return new Vector3(this.z, this.z, this.x); }
+    get zzy() { return new Vector3(this.z, this.z, this.y); }
+    get zzz() { return new Vector3(this.z, this.z, this.z); }
+
+    /* Math Functions *******/
+    add(b) { this.x += b.x; this.y += b.y; this.z +=b.z; return this; }
+    sub(b) { this.x -= b.x; this.y -=b.y; this.z -=b.z; return this; }
+    subtract(b) { return this.sub(b); }
+    scale(b) { this.x *= b; this.y *= b; this.z *=b; return this; }
+    mul(b) { this.x *= b.x; this.y *= b.y; this.z *=b.z; return this; }
+    multiply(b) { return this.mul(b); }
+    div(b) { this.x /= b.x; this.y /= b.y; this.z /=b.z; return this; }
+    divide(b) { return this.div(b); }
+    exp(b) { this.x **= b.x; this.y **= b.y; this.z **=b.z; return this; }
+    pow(b) { return this.exp(b); }
+    len() { return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z ); }
+    length() { return this.len(); }
+    dist(b) { return this.sub(b).len(); }
+    distance(b) { return this.distance(b); }
+    norm() { let l = this.len(); return this.div(new Vector3(l, l, l)); }
+    normalize() { return this.norm(); }
+    dot(b) { return this.x * b.x + this.y * b.y + this.z * b.z; }
+    lerp(b, t) { return this.add(b.sub(this).scale(t)); }
+    interpolate(b, t) { return this.lerp(b, t); }
+    cross(b) { this.x = this.y * b.z - this.z * b.y; this.y = this.z * b.x - this.x * b.z; this.z = this.x * b.y - this.y * b.x; return this; }
+    reflect(b) { let p = 2 * this.dot(b); return this.sub(b.scale(p)); }
+
+    /* Non-Math Functions *******/
+    toArray() { return [this.x, this.y, this.z]; }
+    toString() { return ("(" + this.x + ", " + this.y + ", " + this.z + ")"); }
+    set(x, y, z) { this.x = x; this.y = y; this.z = z; }
+}
+vector3 = Vector3; // Aliases
+Vec3 = Vector3;
+vec3 = Vector3;
+
+/* Array Functions */
+Array.prototype.toVector = function() { if (this.length == 2) { return new Vector2( this[0], this[1] ); } else if (this.length == 3) { return new Vector3( this[0], this[1], this[2] ); } else { console.error("Invalid Array Length"); return(0); } }
